12b1362096ef7adb12bb4f6ba8d34d29c8a85236
Lina <lina.aitbrahim.16@gmail.com>
2026-01-08 20:50:03 +0100

    Initial: Import boilerplate code

---
+++ src/db/memory.js
@@ -0,0 +1,16 @@
+
+// a list of users sorted by username.
+// username must be unique in the list.
+const users = [{ id: 'bf745dd5-43bd-42b0-bf7d-25a4465a8134', username: 'bob', age: 20 }];
+
+// addUser inserts newUser in the user list.
+const addUser = (newUser) => {
+    // TO IMPLEMENT
+}
+
+// getUsers returns a copy of the users.
+const getUsers = () => {
+    // TO IMPLEMENT
+}
+
+// ANOTHER FUNCTIONS COULD BE NECESSARY
